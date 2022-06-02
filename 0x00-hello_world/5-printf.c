f1d625c814836fe78acfc5fd4c47bf7ec89dc8ca
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-03 00:54:27 +0100

    use function printf to display text

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print text on standard output with function printf
+ * Return: 0 to say everything is done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
