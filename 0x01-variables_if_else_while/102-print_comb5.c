cb9cff5cb29247d16c38c1fbcae72fd35fa450a7
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-06 03:50:57 +0100

    prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Print combinaison of two two-digit numbers
+ * Return: 0 to say everything is done
+ */
+
+int main(void)
+{
+	int num_o, num_t, num_th, num_fo;
+
+	for (num_o = 0; num_o <= 9; num_o++)
+	{
+		for (num_t = 0; num_t <= 8; num_t++)
+		{
+			for (num_th = num_o; num_th <= 9; num_th++)
+			{
+				for (num_fo = (num_th == num_o) ? num_t + 1 : 0; num_fo <= 9; num_fo++)
+				{
+					putchar(num_o + 48);
+					putchar(num_t + 48);
+					putchar(' ');
+					putchar(num_th + 48);
+					putchar(num_fo + 48);
+					if (num_o != 9 || num_t != 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
