dc3392361954c6c273f348cb0ce654cb10a84a45
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 22:04:22 +0100

    fix errors display

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,24 +17,24 @@ void times_table(void)
 		{
 			if (num_o * num_t >= 10)
 			{
-				_putchar((num_o * num_t) / 10 + '0');
-				_putchar((num_o * num_t) % 10 + '0');
-				if (num_t < 9)
+
+				if (num_t < 9 && num_o != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
+				_putchar((num_o * num_t) / 10 + '0');
+				_putchar((num_o * num_t) % 10 + '0');
 			}
 			else
 			{
-				_putchar(num_o * num_t + '0');
-				if (num_t < 9)
+				if (num_t < 9 && num_o != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
-
+				_putchar(num_o * num_t + '0');
 			}
 		}
 		_putchar('\n');
