0cb33711e44ca45031b5cad7b6237f64c15305c5
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 20:14:34 +0100

    fix coding style errors

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 /**
  * _abs - check if character is lowercase
  * @val: value to be checked to be checked
- * Return:  val if number is pos or nul otherwise -val 
+ * Return:  val if number is pos or nul otherwise -val
  */
 
 int _abs(int val)
