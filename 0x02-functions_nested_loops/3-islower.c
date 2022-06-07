e8de1736647ef0684c33a3d64e1a859320ab082a
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 19:24:06 +0100

    verify character case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check if character is lowercase
+ * @c: caracter to be checked
+ * Return: 1 if character is lowercase otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
