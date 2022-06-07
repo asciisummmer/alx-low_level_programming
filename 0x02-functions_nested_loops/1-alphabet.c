d54227763db45a3eddf7ea6616808d58804f10e0
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 15:56:41 +0100

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - display alphabet
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
