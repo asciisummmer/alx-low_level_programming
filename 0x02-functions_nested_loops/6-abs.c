b8e13b50a07aa4dfb4f138ab6751c292064c232a
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 20:08:07 +0100

    function to get absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - check if character is lowercase
+ * @val: value to be checked to be checked
+ * Return:  val if number is pos or nul otherwise -val 
+ */
+
+int _abs(int val)
+{
+	if (val >= 0)
+		return (val);
+	return (-val);
+}
