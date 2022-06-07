5a5a798303f910243cba94d5418ad47bd801704c
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 22:12:44 +0100

    sum two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: value to be checked to be checked
+ * @b: value to be checked to be checked
+ * Return:  sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
