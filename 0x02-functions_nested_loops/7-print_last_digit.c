26aa5ed94c9db84a1aab8c996042233f03dd570d
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-07 20:30:43 +0100

    update header file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _abs - print last digit
+ * print_last_digit - print last digit
  * @val: value to print last digit
  * Return:  val if number is pos or nul otherwise -val
  */
@@ -8,6 +8,7 @@
 int print_last_digit(int val)
 {
 	if (val >= 0)
-		return (val % 10);
-	return ((-val) % 10);
+		_putchar(val % 10 + '0');
+	else
+		_putchar((-val) % 10 + '0');
 }
