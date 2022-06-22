7f0a0932c4132b044b81d1caccb21a90e6fe2701
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-22 16:49:27 +0100

    compute factorial number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,26 +10,7 @@ int factorial(int n)
 	if (n < 0)
 		return (-1);
 	if (n <= 1)
-	{
 		return (1);
-	}
 	else
-	{
-		return n * factorial(n - 1);
-	}
-}
-
-int main(void)
-{
-    int r;
-
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+		return (n * factorial(n - 1));
 }
