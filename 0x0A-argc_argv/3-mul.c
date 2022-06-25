c80682c0ca62790eb950cebd7911b5af0fb61149
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-25 02:56:27 +0100

    multiply arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* main - display name program
+* @argc: size of argv
+* @argv: pointer of array
+* Return: 0 just for success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[]__attribute((unused)))
+{
+	long a, b;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	if (a >= -2147483648 && b >= -2147483648
+	&& a <= 2147483647 && b <= 2147483647)
+		printf("%ld\n", a * b);
+	return (EXIT_SUCCESS);
+}
