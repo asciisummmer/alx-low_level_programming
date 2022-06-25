3661d4c4efd65d14c5f87ebe06cfc687a4cd7395
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-25 02:27:05 +0100

    display name argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - display name program
+* @argc: size of argv
+* @argv: pointer of array
+* Return: 0 just for success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[]__attribute((unused)))
+{
+    printf("%s\n", argv[0]);
+    return (EXIT_SUCCESS);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef _main_
+#define _main_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
