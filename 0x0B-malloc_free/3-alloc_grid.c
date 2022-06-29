4529075d52f173cc88e76bd0ad833045f80d88e3
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-29 19:47:27 +0100

    fix error manage memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,8 +10,13 @@
 int **alloc_grid(int width, int height)
 {
 	int size = 0;
-	int **array_int = malloc(sizeof(int *) * height);
+	int **array_int = NULL;
 
+	if (height <= 0 || width <= 0)
+		return (NULL);
+	array_int = malloc(sizeof(int *) * height);
+	if (array_int == NULL)
+		return (NULL);
 	for (size = 0; size < height; size++)
 	{
 		array_int[size] = malloc(sizeof(int **) * width);
