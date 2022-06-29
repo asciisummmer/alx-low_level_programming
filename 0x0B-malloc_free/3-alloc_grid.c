d49008ca1cd82e195f09c841d2d1c9f232ba3185
asciisummer <elwis.soviguidi@epitech.eu>
2022-06-29 20:27:51 +0100

    fix error malloc d'ont alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+* free_memory - liberer la memoire
+* @array_int: array to free
+* @height: size of array
+*/
+
+void free_memory(int **array_int, int height)
+{
+	int h = 0;
+
+	if (height == 0)
+	{
+		free(array_int);
+		array_int = NULL;
+	}
+	for(h = 0; h < height; h++)
+	{
+		free(array_int[h]);
+		array_int[h] = NULL;
+	}
+	free(array_int);
+	array_int = NULL;
+}
+
+/**
 * alloc_grid - return size array
 * @height: string to get lenght
 * @width: string to get lenght
@@ -21,7 +45,10 @@ int **alloc_grid(int width, int height)
 	{
 		array_int[i] = malloc(sizeof(int **) * width);
 		if (array_int[i] == NULL)
+		{
+			free_memory(array_int, i);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			array_int[i][j] = 0;
 	}
