d1bd98dca924c0a00a240c3a5abdab3b7d093275
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-08 02:26:38 +0100

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		return;
 	va_start(args, n);
+	if (separator == NULL)
+		return;
 	for (i = 1; i <= n; i++)
 	{
 		value = va_arg(args, int);
