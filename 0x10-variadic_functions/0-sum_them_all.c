f1adb469788ac043f844a4326867d1baf39555b4
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-08 01:46:26 +0100

    implement variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* sum_them_all - sum arguments
+* @n: number of argument
+* Return: summ of variable argument
+*/
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int  sum = 0;
+	unsigned int i = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 1; i <= n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN
+#define _MAIN
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
