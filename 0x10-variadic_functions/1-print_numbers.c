89084470f10a91ab23f9e12f2707d3384c696939
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-08 02:15:42 +0100

    print all number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers, followed by a new line
+* @separator: separator to print betwen two numbers
+* @n: number of argument
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, value = 0;
+
+	if (n == 0)
+		return;
+	va_start(args, n);
+	for (i = 1; i <= n; i++)
+	{
+		value = va_arg(args, int);
+		printf("%d", value);
+		if (separator != NULL && i != n)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
