cd9ed646812fbd8d6ed7a341a860d5dd2998b6b2
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-12 02:46:32 +0100

    Fix error pointer equal NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,5 +9,41 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+	if (f != NULL)
+	{
+		f(name);
+	}
+	else
+	{
+		f = put_str;
+		f(name);
+	}
+}
+
+/**
+* put_str - display string
+* @str: string to print
+* Return: Nothing.
+*/
+
+void put_str(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+* _putchar - display string
+* @c: display one caracter
+* Return: Nothing.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 void print_name(char *name, void (*f)(char *));
+void put_str(char *str);
+int _putchar(char c);
 
 #endif
