6b04c303112912389d8f6e0edc7257348448f943
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-12 12:52:15 +0100

    fix coding style

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
 
-/*
+/**
 * array_iterator - executes a function given as a parameter
-* on each element of an array.
 * @array: array of int
 * @size: size of the array
 * @action: pointer to the function to execute on element of array
-* @Return: Nothing.
+* Return: Nothing.
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
