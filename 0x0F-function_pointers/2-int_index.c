10c47b448d58fefcf5e3055e2d789fe54811a68e
asciisummer <elwis.soviguidi@epitech.eu>
2022-07-18 17:07:19 +0100

    get index of element in array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+* int_index - executes a function given as a parameter
+* @array: array of int
+* @size: size of the array
+* @cmp: pointer to function for compare elements
+* Return: -1 if no element matches else return index of element found.
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	for (i = 0; i < size && size > 0; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
