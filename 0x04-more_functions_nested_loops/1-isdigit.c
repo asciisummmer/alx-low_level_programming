f8362226baefe7d13bec7a6eb8192d877f6ae79e
asciisummer <romelsoviguidi@gmail.com>
2022-07-21 01:39:31 +0100

    check if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - check if  is digit
+ * @c: number to check
+ * Return:  1 if number is digit otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
