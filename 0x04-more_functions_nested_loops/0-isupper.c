16fa6ed064d74e794d1501e93cab8ac620214909
asciisummer <romelsoviguidi@gmail.com>
2022-07-21 01:34:11 +0100

    function verified character is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if character is uppercase
+ * @c: character to be checked
+ * Return:  1 if character is uppercase otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
