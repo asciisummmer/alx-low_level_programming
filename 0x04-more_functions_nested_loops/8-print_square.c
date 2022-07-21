c7b2d59b3bbca71e0d13ba483a1b1a3d311ecaa9
asciisummer <romelsoviguidi@gmail.com>
2022-07-21 12:14:57 +0100

    print sqare

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square
+ * @size: siwe of square
+ * Return:  Nothing.
+ */
+
+void print_square(int size)
+{
+	int charac, i = 0;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; size > 0 && i <= size; i++)
+	{
+		for (charac = 1; charac <= size; charac++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
