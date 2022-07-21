ecec9986350e8b4d182d1211a9b0c4b27064b373
asciisummer <romelsoviguidi@gmail.com>
2022-07-21 01:45:52 +0100

    multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - mltiply two integer
+ * @a: number 
+ * @b: number
+ * Return:  a x b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
-
+int mul(int a, int b);
 
 int _putchar(char c);
 void print_alphabet(void);
