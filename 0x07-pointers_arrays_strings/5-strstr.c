49f5b7f03ad08a0546a1a0141dde5e38a827f5d0
asciisummer <elwis.soviguidi@epitech.eu>
2023-10-18 19:43:50 +0100

    fix null byte search

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,27 +16,22 @@ char *_strstr(char *haystack, char *needle)
 	int i, j = 0;
 	int not_in = 0;
 
+	if (*needle == '\0' )
+		return (NULL);
 	for (i = 0; *(haystack + i); i++)
 	{
-			if (haystack[i] == needle[0])
+		for (j = 0; *(needle + j); j++)
+		{
+			if (*(needle + j) != *(haystack + i + j))
 			{
-				for (j = 0; *(needle + j); j++)
-				{
-					if (*(needle + j) != *(haystack + i + j))
-					{
-							not_in = 1;
-							break;
-					}
-				}
-				if (not_in)
-				{
-					not_in = 0;
-				}
-				else
-				{
-					return (&haystack[i]);
-				}
+				not_in = 1;
+				break;
 			}
+		}
+		if (not_in)
+			not_in = 0;
+		else
+			return (&haystack[i]);
 		}
 	return (NULL);
 }
