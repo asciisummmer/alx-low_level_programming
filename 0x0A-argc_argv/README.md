6533394a1dc1a2ca9f4a56736d0b0423c83edde9
asciisummer <elwis.soviguidi@epitech.eu>
2023-10-19 14:50:16 +0100

    display prog name

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# alx-low_level_programming
+Learn Low Level  Programming
+## argc and argv in Language C
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
+#include <stdlib.h> 
+
+#endif
+
