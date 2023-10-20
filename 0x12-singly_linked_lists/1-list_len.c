3c064efd9c92ea3b3b0b8d32122e6961b43bce03
asciisummer <elwis.soviguidi@epitech.eu>
2023-10-20 02:55:43 +0100

    update list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,56 +1,6 @@
 #include "lists.h"
 
 /**
- * print_number - print number
- * @n: number to print
- */
-
-void print_number(int n)
-{
-	char c;
-
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		print_number(n / 10);
-
-	c = (n % 10) + 48;
-	write(1, &c, 1);
-}
-
-/**
- * _strlen - size of string
- * @s: string to check
- *
- * Return: len of string
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	for (i = 0; *(s + i); ++i)
-	{
-
-	}
-	return (i);
-}
-
-/**
- * _putstr - size of string
- * @fd: standard output to write
- * @s: string to write
- * @len: size of string
- */
-
-void _putstr(int fd, char *s, int len)
-{
-	write(fd, s, len);
-}
-/**
  * list_len - get number of node in single linked list
  * @h: single linked list
  *
@@ -69,4 +19,3 @@ size_t list_len(const list_t *h)
 	}
 	return (size_node);
 }
-
