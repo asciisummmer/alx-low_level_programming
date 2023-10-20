d82f78401f9ae9ddc429e4418b0c87aadcc6b854
asciisummer <elwis.soviguidi@epitech.eu>
2023-10-20 03:59:15 +0100

    bonus mm

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void print_phrase(void) __attribute__((constructor));
  * print_phrase - print phrase before main
  */
 
-void print_phrase(void)
+void print_phrase()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my ");
 	printf("house upon my back!\n");
