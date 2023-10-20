76ed870107b8ab30a873d0d5c4bc4fa236aeadbb
asciisummer <elwis.soviguidi@epitech.eu>
2023-10-20 04:04:42 +0100

    bonus fix error coding

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@ void print_phrase(void) __attribute__((constructor));
  * print_phrase - print phrase before main
  */
 
-void print_phrase()
+void print_phrase(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my ");
 	printf("house upon my back!\n");
