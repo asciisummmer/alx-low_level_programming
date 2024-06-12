295446be296aabfa37401796652d6cbdb968977e
asciisummer <romelsoviguidi@gmail.com>
2024-06-12 17:53:36 +0100

    feat: print binary number with shift operator

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - print representation binary of number
+ * @n: number to print representary
+ *
+ * Return: void
+**/
+
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	int is_put_one = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 30; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			is_put_one = 1;
+			_putchar('1');
+		}
+		else if (is_put_one)
+		{
+			_putchar('0');
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,11 @@
 #define _MAIN_h
 
 #include <stddef.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 int is_base_binary(const char *b);
+void print_binary(unsigned long int n);
+void _putchar(char c);
 
 #endif
