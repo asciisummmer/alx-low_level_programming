904f251ae4162c5edff45d299dd2d7740d04db52
asciisummer <romelsoviguidi@gmail.com>
2024-08-16 23:29:28 +0100

    implement strncopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,46 @@
+#include <stddef.h>
+
+#include "main.h"
+
+/**
+ * _strlen - compute size of string
+ * @s: string to get size
+ *
+ * Return: size of string
+*/
+
+int _strlen(char *s)
+{
+	int size = 0;
+
+	if (s == NULL)
+	{
+		return (size);
+	}
+	for (; s[size] != '\0'; size++)
+	{
+	}
+	return (size);
+}
+
+/**
+  * _strncpy - copy  string
+  * @dest: destination of string
+  * @src: source of string
+  * @n: number of character
+  *
+  * Return: pointer to dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (n > _strlen(src))
+	dest[i] = '\0';
+	return (dest);
+}
