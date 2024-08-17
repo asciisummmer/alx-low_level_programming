407714eef54e3c59ab8cd0e34eb8b80123da6eb2
asciisummer <romelsoviguidi@gmail.com>
2024-08-17 22:49:30 +0100

    reverse array of integer

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of int
+ * @a: pointer to array of integer
+ * @n: size of array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp = 0;
+
+	for (; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 + i];
+		a[n - 1 + i] = temp;
+	}
+}
