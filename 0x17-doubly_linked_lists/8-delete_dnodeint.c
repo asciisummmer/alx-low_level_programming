6f9c60b1834c025922c6c8a9b4e8ca931f151ccb
asciisummer <romelsoviguidi@gmail.com>
2024-08-20 17:56:35 +0100

    delete node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,41 @@
 #include "lists.h"
 
 /**
+ * delete_dnodeint_at_index - delete node at index
+ * @head: head of list
+ * @index: index of list
+ *
+ * Return: 1 if delete node else -1
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    
+	dlistint_t *tmp = NULL, *copy = NULL;
+	unsigned int current_index = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	while (tmp != NULL)
+	{
+		if (current_index == index)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			copy = tmp;
+			free(copy);
+			return (1);
+		}
+		tmp = tmp->next;
+		current_index++;
+	}
+	return (-1);
 }
