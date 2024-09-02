11f21a82d8994ff471a7bcc54e94749781018eaa
asciisummer <romelsoviguidi@gmail.com>
2024-09-02 16:41:45 +0100

    add node to hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,12 +10,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
 
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
 	hash_table->size = size;
 	return (hash_table);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,65 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - set hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if operation is doing else 0
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index = key_index((const unsigned char *)key, 1024);
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL || _strlen(key) == 0)
+		return (0);
+	node->key = _strdup(key);
+	node->value = _strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
+
+/**
+ * _strdup - dup string
+ * @str: str to dup
+ *
+ * Return: address to new copy
+*/
+
+char *_strdup(const char *str)
+{
+	int i = 0;
+	char *copy = NULL;
+
+	for (; str[i] != '\0'; i++)
+	{}
+	copy = malloc(sizeof(char) * i + 1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		copy[i] = str[i];
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+/**
+ * _strlen - len of string
+ * @str: string
+ *
+ * Return: len of string
+*/
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	if (str == NULL)
+		return (0);
+	for (; str[i] != '\0'; i++)
+	{}
+	return (i);
+}
