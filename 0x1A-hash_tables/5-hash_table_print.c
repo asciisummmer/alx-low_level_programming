7e025f47db52a514019a1c8af632c578c458c509
asciisummer <romelsoviguidi@gmail.com>
2024-09-03 11:06:06 +0100

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash_table key and value
+ * @ht: hash table to print
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *tmp = NULL;
+
+	if (ht == NULL || ht->array == NULL)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				printf(" %ld '%s': '%s'", i, tmp->key, tmp->value);
+				tmp = tmp->next;
+				if (tmp != NULL)
+					printf(", ");
+			}
+		}
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -41,5 +41,5 @@ int _strlen(const char *str);
 int _strcmp(const char *str1, const char *str2);
 int check_key_update(hash_node_t *head, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
+void hash_table_print(const hash_table_t *ht);
 #endif
