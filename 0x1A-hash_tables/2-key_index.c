daf71ba119cbaaa6d2f5fb4c4d50e6616375fad9
asciisummer <romelsoviguidi@gmail.com>
2024-09-03 17:12:15 +0100

    fix: cast index in ling int

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	int index = hash_djb2(key);
+	unsigned long int index = hash_djb2(key);
 
 	return (index % size);
 }
