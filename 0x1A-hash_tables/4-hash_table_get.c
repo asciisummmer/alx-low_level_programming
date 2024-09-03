6574645e52691d727a8c843fc344bfc479541ca4
asciisummer <romelsoviguidi@gmail.com>
2024-09-03 10:21:17 +0100

    add string library

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,45 +8,25 @@
  * Return: value if operation is doing else NULL
 */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	int index = 0;
+	hash_node_t *tmp = NULL;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL || _strlen(key) == 0)
 		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	if (ht->array[index] == NULL)
+		return (NULL);
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (_strcmp(tmp->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
-			return (1);
+			return (tmp->value);
 		}
+		tmp = tmp->next;
 	}
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-
-	return (1);
+	return (NULL);
 }
